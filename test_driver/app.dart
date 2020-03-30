e477233e1be3c2a0e8731ea992ee5640dc62762b
Taka Watanabe <62856945+taka-wtnb@users.noreply.github.com>
2020-03-29 17:48:32 -0700

    Update app.dart
    Update the author's name from "Takahiro" to "Taka"

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,4 +1,4 @@
-// Author: Takahiro Watanabe
+// Author: Taka Watanabe
 // Program Name: CS492 (Winter 2020) Project 5: Wasteagram / app.dart file
 // Description: This file will used for the integration test of this app's some UX flows
 // Source: Lecture videos from Week 9 and Week 10 modules in CS492 (Winter 2020)
