7c46e55131202a86189b75370112599fea18e4c4
Taka Watanabe <62856945+taka-wtnb@users.noreply.github.com>
2020-03-29 17:54:08 -0700

    Update photo_screen.dart
    Update the author's first name from "Takahiro" to "Taka"

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,4 +1,4 @@
-// Author: Takahiro Watanabe
+// Author: Taka Watanabe
 // Program Name: CS492 (Winter 2020) Project 5: Wasteagram / photo_screen.dart file
 // Description: This file implements the route to be used for the image picker. 
 // Source: Lecture videos from Week 9 and Week 10 modules in CS492 (Winter 2020)
