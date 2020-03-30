34c603acd69f4f0f4a68a863315df25008db5eb1
Taka Watanabe <62856945+taka-wtnb@users.noreply.github.com>
2020-03-29 17:52:14 -0700

    Update food_waste_post_list.dart
    Update the author's first name from "Takahiro" to "Taka"

--- lib/models/food_waste_post_list.dart
+++ lib/models/food_waste_post_list.dart
@@ -1,4 +1,4 @@
-// Author: Takahiro Watanabe
+// Author: Taka Watanabe
 // Program Name: CS492 (Winter 2020) Project 5: Wasteagram / food_waste_post_list.dart file
 // Description: This file implements the FoodWastePostList class, which is a model to
 //              represent a list of food waste posts.
